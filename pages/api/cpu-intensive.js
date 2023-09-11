1e5e6e9db49e1619bc4d2f2a9e33552fa9686c2c
annikel <92825476+annikel@users.noreply.github.com>
2023-09-11 12:54:15 -0700

    Update cpu-intensive.js

--- pages/api/cpu-intensive.js
+++ pages/api/cpu-intensive.js
@@ -8,11 +8,8 @@ export default function handler(req, res) {
     sum += Math.sqrt(i);
   }
 
-  // Add a 30-second delay before sending the response
-  setTimeout(() => {
-    const endTime = Date.now();
-    const elapsedTime = (endTime - startTime) / 1000; // in seconds
+const endTime = Date.now();
+const elapsedTime = (endTime - startTime) / 1000; // in seconds
 
-    res.status(200).json({ message: `CPU-intensive task completed in ${elapsedTime.toFixed(3)} seconds.` });
-  }, 30000); // 30 seconds in milliseconds
+res.status(200).json({ message: `CPU-intensive task completed in ${elapsedTime.toFixed(3)} seconds.` });
 }
