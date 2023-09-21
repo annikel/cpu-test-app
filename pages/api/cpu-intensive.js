679241dfb1e88041868c149b540b481718d20a46
Annina Keller <anninakeller@microsoft.com>
2023-09-21 09:23:09 -0700

    add parameter

--- pages/api/cpu-intensive.js
+++ pages/api/cpu-intensive.js
@@ -1,18 +1,16 @@
-// pages/api/cpu-intensive.js
 export default function handler(req, res) {
   const startTime = Date.now();
   let sum = 0;
-
-  // Adjust loop iterations to target around 500 milliseconds
-  for (let i = 0; i < 20000000; i++) {
-    sum += Math.sqrt(i);
+  
+  let compute = req.query.compute;
+  if (compute === 'true') {
+    for (let i = 0; i < 20000000; i++) {
+      sum += Math.sqrt(i);
+    }
   }
 
-  // Add a 30-second delay before sending the response
-  setTimeout(() => {
-    const endTime = Date.now();
-    const elapsedTime = (endTime - startTime) / 1000; // in seconds
+  const endTime = Date.now();
+  const elapsedTime = (endTime - startTime) / 1000;
 
-    res.status(200).json({ message: `CPU-intensive task completed in ${elapsedTime.toFixed(3)} seconds.` });
-  }, 30000); // 30 seconds in milliseconds
+  res.status(200).json({ message: `CPU-intensive task completed in ${elapsedTime.toFixed(3)} seconds with compute set to ${compute}.` });
 }
